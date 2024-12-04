2ffca1179042c377a56266e3b1bf2e4ce52e32cc
Минтик <121098677+Msakhibullin24@users.noreply.github.com>
2024-12-04 21:51:01 +0300

    Carousel_karting + horse_carusel

---
+++ CAROUSEL_HORSE.py
@@ -0,0 +1,17 @@
+from MAIN import MAIN
+from enum import Enum
+
+
+class Carousel_horse(MAIN):
+    def __init__():
+        super().__init__()
+        self.Carousel_horse= 'состояние карусель лошадки'
+
+    def start_Carousel_horse ():
+        return 'Запуск  именно карусели лошадки'
+
+    def stop_Carousel_horse):
+        return 'Стоп именно карусели лошадка'
+
+    def add_Carousel_horse():
+        return 'добавить время катания на каруселе лошадки'
---
+++ karting.py
@@ -0,0 +1,17 @@
+from MAIN import MAIN
+from enum import Enum
+
+
+class Carousel_karting(MAIN):
+    def __init__():
+        super().__init__()
+        self.Carousel_karting = 'состояние картинка (как аттракциона)'
+
+    def start_Carousel_karting ():
+        return 'Запуск  именно картинга'
+
+    def stop_Carousel_karting():
+        return 'Стоп именно картинг'
+
+    def add_Carousel_karting():
+        return 'добавить время катания на картах'
